059427339c975a734b67515ea3a424c97a65d1b0
David Mlnarik <david.mlnarik@blackboard.com>
2019-07-22 23:06:37 +0200

    port change test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,6 @@ class MyApp extends App {
         <Head>
           <title>Svatební floristika - Miroslava Blatná</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
           <link rel="icon" type="image/png" href="/static/logo.png" />
         </Head>
         <Header />

267b310bc42f9188607d02947304ab2a722ce0b5
David Mlnarik <david.mlnarik@blackboard.com>
2019-07-26 22:57:19 +0200

    optimized images for index page

--- components/content/ServicesList.tsx
+++ components/content/ServicesList.tsx
@@ -68,11 +68,10 @@ const InfoHeaderH3 = styled.h3`
 export const ServicesList = () => (
     <>
         <FrameDiv>
-            <Image src="/static/gallery/6.jpg" />
-
+            <Image src="/static/index/01.jpg" />
         </FrameDiv>
         <FrameDiv>
-            <Image src="/static/gallery/9.jpg" />
+            <Image src="/static/index/02.jpg" />
             <InfoDiv>
             <InfoHeaderH3>Osobitý přístup</InfoHeaderH3>
                 Floristikou se zabývám už několik let a je mou velkou zálibou a koníčkem.
@@ -82,20 +81,19 @@ export const ServicesList = () => (
             </InfoDiv>
         </FrameDiv>
         <FrameDiv>
-            <Image src="/static/gallery/11.jpg" />
+            <Image src="/static/index/03.jpg" />
             <InfoDiv>
             <InfoHeaderH3>Spokojenost je předností</InfoHeaderH3>
                 Mojí snahou je docílit vždy maximální spokojenosti zákazníka i s ohledem na příznivou cenu všech vazeb.
                 A to s láskou a citem pro detail a kompozici.
                 Vázání svatebních kytic představuje pro mne velikou radost a potěšení.                
-
             </InfoDiv>            
         </FrameDiv>
         <FrameDiv>
-            <Image src="/static/gallery/12.jpg" />
+            <Image src="/static/index/04.jpg" />
         </FrameDiv>
         <BigFrameDiv>
-            <Image src="/static/gallery/5.jpg" />
+            <Image src="/static/index/05.jpg" />
             <InfoDiv>
                 <InfoHeaderH3>Vyšperkování do posledního detailu</InfoHeaderH3>
                 <p>
@@ -110,26 +108,26 @@ export const ServicesList = () => (
             </InfoDiv>
         </BigFrameDiv>
         <FrameDiv>
-            <Image src="/static/gallery/3.jpg" />
+            <Image src="/static/index/06.jpg" />
             <InfoDiv>
                 <InfoHeaderH3>Obřad v Botanické zahradě</InfoHeaderH3>
                 Jako zaměstnanec Botanické zahrady a arboreta Mendelovy univerzity v Brně, jsem Vám schopna zajistit svatební obřad v krásném prostředí botanické zahrady.
             </InfoDiv>
         </FrameDiv>        
         <FrameDiv>
-            <Image src="/static/gallery/4.jpg" />
+            <Image src="/static/index/07.jpg" />
         </FrameDiv>
         <SmallFrameDiv>
-            <Image src="/static/gallery/7.jpg" />
+            <Image src="/static/index/08.jpg" />
         </SmallFrameDiv>
         <SmallFrameDiv>
-            <Image src="/static/gallery/8.jpg" />
+            <Image src="/static/index/09.jpg" />
         </SmallFrameDiv>
         <SmallFrameDiv>
-            <Image src="/static/gallery/2.jpg" />
+            <Image src="/static/index/10.jpg" />
         </SmallFrameDiv>
         <SmallFrameDiv>
-            <Image src="/static/gallery/1.jpg" />
+            <Image src="/static/index/11.jpg" />
         </SmallFrameDiv>      
         <Link href="/galerie">
             <Button fluid style={{margin: 0}} size='big'>Podívat se do galerie na více fotek</Button>

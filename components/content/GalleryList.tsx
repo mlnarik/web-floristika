fff2d466cc0778cceff8a8f061f73b8c6583e462
David Mlnarik <david.mlnarik@blackboard.com>
2019-07-27 15:52:11 +0200

    added gallery images

--- components/content/GalleryList.tsx
+++ components/content/GalleryList.tsx
@@ -6,15 +6,19 @@ export const GalleryList = () => (
         <Img previewable src="/static/gallery/7.jpg" />
         <Img previewable src="/static/gallery/8.jpg" />
         <Img previewable small src="/static/gallery/9.jpg" />
-        <Img previewable small src="/static/gallery/11.jpg" />
-        <Img previewable small src="/static/gallery/3.jpg" />
-        <Img previewable small src="/static/gallery/4.jpg" />
-        <Img previewable src="/static/gallery/1.jpg" />
-        <Img previewable src="/static/gallery/2.jpg" />
         <Img previewable small src="/static/gallery/10.jpg" />
+        <Img previewable small src="/static/gallery/1.jpg" />
+        <Img previewable small src="/static/gallery/3.jpg" />
+        <Img previewable src="/static/gallery/2.jpg" />
+        <Img previewable src="/static/gallery/4.jpg" />
         <Img previewable small src="/static/gallery/5.jpg" />
         <Img previewable small src="/static/gallery/6.jpg" />
+        <Img previewable small src="/static/gallery/11.jpg" />
         <Img previewable small src="/static/gallery/12.jpg" />
+        <Img previewable small src="/static/gallery/13.jpg" />
+        <Img previewable small src="/static/gallery/14.jpg" />
+        <Img previewable small src="/static/gallery/15.jpg" />
+        <Img previewable small src="/static/gallery/16.jpg" />
         <ContactNow />
     </>
 )
